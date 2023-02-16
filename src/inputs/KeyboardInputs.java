e54ff2285740adf164f66bc927a57e7d14817318
davidventasmarin <dvm@medlinetec.eu>
2023-02-16 14:29:36 +0100

    Terminamos el video 05 de animaciones, y hemos creado además del array con todos los sprites separados por movimiento, hemos creado las constantes para tenerlo todo automatizado, además de permitir el movimiento con el teclado añadiendo desplazamientos.

--- src/inputs/KeyboardInputs.java
+++ src/inputs/KeyboardInputs.java
@@ -5,6 +5,8 @@ import java.awt.event.KeyListener;
 
 import main.GamePanel;
 
+import static utils.Constants.Directions.*;
+
 public class KeyboardInputs implements KeyListener{
 
 
@@ -22,23 +24,30 @@ public class KeyboardInputs implements KeyListener{
     public void keyPressed(KeyEvent e){
         switch(e.getKeyCode()){
             case KeyEvent.VK_W:
-                gamePanel.changeYDelta(-5);
+                gamePanel.setDirection(UP);;
                 break;
             case KeyEvent.VK_A:
-                gamePanel.changeXDelta(-5);
+                gamePanel.setDirection(LEFT);
                 break;
             case KeyEvent.VK_S:
-                gamePanel.changeYDelta(5);
+                gamePanel.setDirection(DOWN);
                 break;
             case KeyEvent.VK_D:
-                gamePanel.changeXDelta(5);
+                gamePanel.setDirection(RIGHT);
                 break;
         }
     }
 
     @Override
     public void keyReleased(KeyEvent e){
-
+        switch(e.getKeyCode()){
+            case KeyEvent.VK_W:
+            case KeyEvent.VK_A:
+            case KeyEvent.VK_S:
+            case KeyEvent.VK_D:
+                gamePanel.setMoving(false);
+                break;
+        }
     }
     
 }
--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -12,13 +12,19 @@ import javax.swing.JPanel;
 import inputs.KeyboardInputs;
 import inputs.MouseInputs;
 
+import static utils.Constants.PlayerConstants.*;
+import static utils.Constants.Directions.*;
+
 
 public class GamePanel extends JPanel{
     MouseInputs mouseInputs = new MouseInputs(this);
     private float xDelta = 100, yDelta = 100;
     private BufferedImage img;
-    private BufferedImage[] idleAnimation;
+    private BufferedImage[][] animations;
     private int animationTick, animationIndex, animationSpeed = 15;
+    private int playerAction = IDLE;
+    private int playerDir = -1;
+    private boolean moving = false;
 
     public GamePanel(){
         addKeyListener(new KeyboardInputs(this));
@@ -49,12 +55,13 @@ public class GamePanel extends JPanel{
         // Para saber el tamaño del array, debo de saber con antelación
         // cuantas animaciones voy a tener. En este caso, mirando el png
         // vemos que la primera animación va a tener 5 idle (imágenes)
-        idleAnimation = new BufferedImage[5];
+        animations = new BufferedImage[9][6];
 
-        for(int index = 0; index < idleAnimation.length; index++){
-            idleAnimation[index] = img.getSubimage(index*64, 0, 64, 40);
+        for(int j = 0; j < animations.length; j++){
+            for(int index = 0; index < animations[j].length; index++){
+                animations[j][index] = img.getSubimage(index*64, j*40, 64, 40);
+            }
         }
-
     }
 
     private void setPanelSize() {
@@ -62,24 +69,22 @@ public class GamePanel extends JPanel{
         setPreferredSize(size);
     }
 
-    public void changeXDelta(int value){
-        this.xDelta += value;
+    public void setDirection(int direction){
+        this.playerDir = direction;
+        moving = true;
     }
 
-    public void changeYDelta(int value){
-        this.yDelta += value;
-    }
-
-    public void setRectPos(int x, int y){
-        this.xDelta = x;
-        this.yDelta = y;
+    public void setMoving(boolean moving){
+        this.moving = moving;
     }
 
     public void paintComponent(Graphics g){
         super.paintComponent(g);
 
         updateAnimationTick();
-        g.drawImage(idleAnimation[animationIndex], (int) xDelta, (int) yDelta, 128, 80, null);
+        setAnimation();
+        updatePos();
+        g.drawImage(animations[playerAction][animationIndex], (int) xDelta, (int) yDelta, 256, 160, null);
     }
 
     public void updateAnimationTick(){
@@ -87,8 +92,35 @@ public class GamePanel extends JPanel{
         if(animationTick >= animationSpeed){
             animationTick = 0;
             animationIndex++;
-            if(animationIndex >= idleAnimation.length){
+            if(animationIndex >= GetSpriteAmount(playerAction)){
                 animationIndex = 0;
+            }
+        }
+    }
+
+    public void setAnimation(){
+        if(moving){
+            playerAction = RUNNING;
+        }else{
+            playerAction = IDLE;
+        }
+    }
+
+    public void updatePos(){
+        if(moving){
+            switch(playerDir){
+                case LEFT:
+                    xDelta -= 5; 
+                    break;
+                case UP:
+                    yDelta -= 5;
+                    break;
+                case RIGHT:
+                    xDelta += 5;
+                    break;
+                case DOWN:
+                    yDelta += 5;
+                    break;
             }
         }
     }
