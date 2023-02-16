a59830c90b7d24a75c7bb60ffcd9839f917a42b5
davidventasmarin <dvm@medlinetec.eu>
2023-02-16 12:38:49 +0100

    Puesto el primer array con bufferedImage para conseguir todos los  c:; cd 'c:\Users\medlinetec\Desktop\Pruebas_de_codigo\ComoProgramarEnJava\PruebasJava\GameProject\GameProject'; & 'C:\Program Files\AdoptOpenJDK\jdk-11.0.11.9-openj9\bin\java.exe' '-cp' 'C:\Users\medlinetec\Desktop\Pruebas_de_codigo\ComoProgramarEnJava\PruebasJava\GameProject\GameProject\bin' 'main.MainClass'

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -3,6 +3,7 @@ package main;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.io.InputStream;
 
 import javax.imageio.ImageIO;
@@ -15,19 +16,16 @@ import inputs.MouseInputs;
 public class GamePanel extends JPanel{
     MouseInputs mouseInputs = new MouseInputs(this);
     private float xDelta = 100, yDelta = 100;
-    private BufferedImage img, subImg;
+    private BufferedImage img;
+    private BufferedImage[] idleAnimation;
 
     public GamePanel(){
         addKeyListener(new KeyboardInputs(this));
         importImg();
+        loadAnimation();
         setPanelSize();
         addMouseListener(mouseInputs);
         addMouseMotionListener(mouseInputs);
-    }
-
-    private void setPanelSize() {
-        Dimension size = new Dimension(1280, 800);
-        setPreferredSize(size);
     }
 
     private void importImg(){
@@ -35,9 +33,32 @@ public class GamePanel extends JPanel{
 
         try {
             img = ImageIO.read(is);
-        } catch (Exception e) {
+        } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            try {
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
+    }
+
+    private void loadAnimation(){
+        // Para saber el tamaño del array, debo de saber con antelación
+        // cuantas animaciones voy a tener. En este caso, mirando el png
+        // vemos que la primera animación va a tener 5 idle (imágenes)
+        idleAnimation = new BufferedImage[5];
+
+        for(int index = 0; index < idleAnimation.length; index++){
+            idleAnimation[index] = img.getSubimage(index*64, 0, 64, 40);
+        }
+
+    }
+
+    private void setPanelSize() {
+        Dimension size = new Dimension(1280, 800);
+        setPreferredSize(size);
     }
 
     public void changeXDelta(int value){
@@ -56,8 +77,7 @@ public class GamePanel extends JPanel{
     public void paintComponent(Graphics g){
         super.paintComponent(g);
 
-        subImg = img.getSubimage(1*64, 8*40, 64, 40);
-        g.drawImage(subImg, (int) xDelta, (int) yDelta, 128, 80, null);
+        g.drawImage(idleAnimation[2], (int) xDelta, (int) yDelta, 128, 80, null);
     }
 
 }
